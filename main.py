66241d35952c3cacd0c18bc1442ecfae4bc13bdc
khengari77 <khengari77@gmail.com>
2023-07-15 09:02:15 +0200

    Adding more necessary files

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     sleep_tracker = SleepTracker(capture.frame)
     while True:
         sleep_tracker.take(capture.frame)
-        viewer.frame = capture.frame
+        viewer.take(capture.frame, {})
         print(f"awareness_level: {sleep_tracker.awareness_level:.3f}")
         if capture.stopped or viewer.stopped:
             viewer.stop()
