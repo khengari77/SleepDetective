47cc99edeb7d9f0a14b16c04f6d662aa83f8c2a9
khengari77 <khengari77@gmail.com>
2025-03-23 01:42:34 +0200

    Manage pin state

--- modules/ActionTaker.py
+++ modules/ActionTaker.py
@@ -7,20 +7,21 @@ class ActionTaker:
 
     def __init__(self, use_gpio=True, use_gsm=True, gps_port="/dev/ttyS4"):
         self.stopped = False
-        self.pin_state = False
-        self.last_state = False        
+        self.pin1_state = False
+        self.pin2_state = False
         self.message_sent = False
         self.gps_port = gps_port 
         self.use_gpio = use_gpio
         self.use_gsm = use_gsm
 
-    def take(self, state):
-        self.last_state = self.pin_state
-        self.pin_state = state
+    def take(self, awareness_level):
+        self.pin1_state = awareness_level < 0.8
+        self.pin2_state = awareness_level < 0.7
 
     def start(self):
         if self.use_gpio:
-            self.pin = GPIO("/dev/gpiochip0", 17, "out")
+            self.pin1 = GPIO("/dev/gpiochip0", 17, "out")
+            self.pin2 = GPIO("/dev/gpiochip0", 16, "out")
         if self.use_gsm:
             self.gsm  = GSM(port=self.gps_port)
         Thread(target=self.action, args=()).start()
@@ -28,13 +29,7 @@ class ActionTaker:
 
     def action(self):
         while not self.stopped:
-            if self.last_state and not self.pin_state:
-                time.sleep(5)
-            elif self.last_state and self.pin_state and not self.message_sent:
-                self.gsm.send_SMS("+218944682876", "HELP!")
-            else:
-                self.message_sent = False
-            self.pin.write(self.pin_state)
+            self.pin1.write(self.pin1_state)
     def stop(self):
         self.pin.write(False)
         self.stopped = True
